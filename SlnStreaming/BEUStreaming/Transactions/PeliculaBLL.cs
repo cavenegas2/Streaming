7279560e37a97ec17ceb48c9002d9b977922875f
cristian venegas <cavenegas2@espe.edu.ec>
2020-06-11 11:49:33 -0500

    Agregado lo enseñado en clase

---
+++ SlnStreaming/BEUStreaming/Transactions/PeliculaBLL.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEUStreaming.Transactions
+{
+   public class PeliculaBLL
+    {
+        //BLL Bussiness Logic Layer
+        //Capa de Logica del Negocio
+
+        public static void Create(Pelicula p)
+        {
+            using (ServicioStreamEntities db = new ServicioStreamEntities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Pelicula.Add(p);
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static Pelicula Get(int? id)
+        {
+            ServicioStreamEntities db = new ServicioStreamEntities();
+            return db.Pelicula.Find(id);
+        }
+
+        public static void Update(Pelicula pelicula)
+        {
+            using (ServicioStreamEntities db = new ServicioStreamEntities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Pelicula.Attach(pelicula);
+                        db.Entry(pelicula).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static void Delete(int? id)
+        {
+            using (ServicioStreamEntities db = new ServicioStreamEntities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        Pelicula pelicula = db.Pelicula.Find(id);
+                        db.Entry(pelicula).State = System.Data.Entity.EntityState.Deleted;
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static List<Pelicula> List()
+        {
+            ServicioStreamEntities db = new ServicioStreamEntities();
+          
+
+            return db.Pelicula.ToList();
+
+            
+        }
+
+
+        private static List<Pelicula> GetAlumnos(string criterio)
+        {
+           
+            ServicioStreamEntities db = new ServicioStreamEntities();
+            return db.Pelicula.Where(x => x.nombre_pelicula.ToLower().Contains(criterio)).ToList();
+        }
+
+        private static Pelicula GetPelicula(string nombre)
+        {
+            ServicioStreamEntities db = new ServicioStreamEntities();
+            return db.Pelicula.FirstOrDefault(x => x.nombre_pelicula == nombre);
+      
+
+    }
+}
+}
---
+++ SlnStreaming/Ejercicio_Streaming_Venegas/Controllers/PeliculasController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BEUStreaming;
+using BEUStreaming.Transactions;
+
+namespace Ejercicio_Streaming_Venegas.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        //private ServicioStreamEntities db = new ServicioStreamEntities();
+
+        // GET: Peliculas
+        public ActionResult Index()
+        {
+            return View(PeliculaBLL.List());
+        }
+
+        // GET: Peliculas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pelicula pelicula = PeliculaBLL.Get(id);
+            if (pelicula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pelicula);
+        }
+
+        // GET: Peliculas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Peliculas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_pelicula,nombre_pelicula,fech_lanz_pelicula,categoria_pelicula,duracion_pelicula")] Pelicula pelicula)
+        {
+            if (ModelState.IsValid)
+            {
+                PeliculaBLL.Create(pelicula);
+                return RedirectToAction("Index");
+            }
+
+            return View(pelicula);
+        }
+
+        // GET: Peliculas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pelicula pelicula = PeliculaBLL.Get(id);
+            if (pelicula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pelicula);
+        }
+
+        // POST: Peliculas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_pelicula,nombre_pelicula,fech_lanz_pelicula,categoria_pelicula,duracion_pelicula")] Pelicula pelicula)
+        {
+            if (ModelState.IsValid)
+            {
+                PeliculaBLL.Update(pelicula);
+                return RedirectToAction("Index");
+            }
+            return View(pelicula);
+        }
+
+        // GET: Peliculas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pelicula pelicula = PeliculaBLL.Get(id);
+            if (pelicula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pelicula);
+        }
+
+        // POST: Peliculas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PeliculaBLL.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        
+    }
+}
